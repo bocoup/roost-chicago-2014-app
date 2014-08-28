1faabbcc29dbfa342e1970b10ece50960fc1a6c7
Ben Alman <cowboy@rj3.net>
2014-08-28 17:25:46 -0400

    Add jshint task, fix code inconsistencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = function(grunt) {
 
   grunt.initConfig({
@@ -6,13 +8,31 @@ module.exports = function(grunt) {
       options: {
         config: '.jscsrc',
       },
-      all: ['*.js', 'src/**/*.js'],
+      all: [
+        '<%= jshint.build.src %>',
+        '<%= jshint.app.src %>',
+      ],
+    },
+    jshint: {
+      build: {
+        options: {
+          jshintrc: '.jshintrc',
+        },
+        src: '*.js',
+      },
+      app: {
+        options: {
+          jshintrc: 'src/.jshintrc',
+        },
+        src: 'src/**/*.js',
+      },
     },
   });
 
   grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Register alias tasks.
-  grunt.registerTask('default', ['jscs']);
+  grunt.registerTask('default', ['jscs', 'jshint']);
 
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+(function() {
+  'use strict';
 
-function awesome() {
-  document.body.innerHTML = '<h1>OMG ROOST</h1>';
-}
+  function awesome() {
+    document.body.innerHTML = '<h1>OMG ROOST</h1>';
+  }
 
-awesome();
+  awesome();
+
+}());
