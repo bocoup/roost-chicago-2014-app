e14c5f46542e051c7f4468f4cfd97f54b7e314d7
Ben Alman <cowboy@rj3.net>
2014-08-28 16:53:24 -0400

    Add jscs task, fix code style inconsistencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,18 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
+    pkg: grunt.file.readJSON('package.json'),
+    jscs: {
+      options: {
+        config: '.jscsrc',
+      },
+      all: ['*.js', 'src/**/*.js'],
+    },
   });
 
+  grunt.loadNpmTasks('grunt-jscs');
+
   // Register alias tasks.
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['jscs']);
 
 };
