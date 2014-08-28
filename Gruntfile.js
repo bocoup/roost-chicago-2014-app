7d2af0f75fb6bc10d5b64e33339a91a0eafe1228
Ben Alman <cowboy@rj3.net>
2014-08-28 16:26:29 -0400

    Add Grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+  });
+
+  // Register alias tasks.
+  grunt.registerTask('default', []);
+
+};
