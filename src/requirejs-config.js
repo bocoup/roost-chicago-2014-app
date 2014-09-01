daa8c40f4375cbbdfe95fb4801e30ee0cdb25852
ashley williams <heyashleyashley@gmail.com>
2014-09-01 13:08:40 -0400

    Setup lo-dash and underscore configuration
    - Add lo-dash template loader to bower.json
    - Add 'tmpl' to require.js config
    - Configure lo-dash to load underscore templates

--- src/requirejs-config.js
+++ src/requirejs-config.js
@@ -6,7 +6,13 @@
     paths: {
       backbone: 'bower_components/backbone/backbone',
       jquery: 'bower_components/jquery/dist/jquery',
-      underscore: 'bower_components/lodash/dist/lodash.underscore'
+      underscore: 'bower_components/lodash/dist/lodash.underscore',
+      tmpl: 'bower_components/lodash-template-loader/loader'
+    },
+    map: {
+      'tmpl': {
+        'lodash': 'underscore'
+      },
     },
 
     deps: ['main']
