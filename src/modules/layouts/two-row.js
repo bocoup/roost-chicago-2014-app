0f1578d15229244a94c6794b185f94814b477550
Isaac Durazo <isaacdurazo@gmail.com>
2014-09-02 14:08:05 -0400

    Make Roostagram pretty

--- src/modules/layouts/two-row.js
+++ src/modules/layouts/two-row.js
@@ -3,9 +3,26 @@ define(function(require) {
 
   var BaseLayout = require('modules/core/base-layout');
   var template = require('tmpl!modules/layouts/two-row');
+  var $ = require('jquery');
+
+  var doc = $(document);
 
   var TwoRowLayout = BaseLayout.extend({
-    template: template
+    template: template,
+
+    afterRender: function() {
+      var topRow = this.$el.find('.top-row');
+
+      var topRowTop = topRow.offset().top;
+
+      function bound(value, min, max) {
+        return Math.min(Math.max(value, min), max);
+      }
+
+      $(window).off('scroll').on('scroll', function() {
+        topRow.css('top', bound(topRowTop - doc.scrollTop(), 0, topRowTop));
+      });
+    }
   });
 
   return TwoRowLayout;
