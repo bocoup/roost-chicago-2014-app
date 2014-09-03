65810369f0c85f49f7667984827b501b3fc21a4f
Mike Pennisi <mike@mikepennisi.com>
2014-09-03 16:45:35 -0400

    Introduce Sinon.JS testing utility
    Use the libraries "spy" objects to improve test maintainability.
    
    Unfortunately, the built library is not hosted on NPM or Bower, so we
    need to manually download the source from the project website and check
    it in to the repository.

--- test/unit/tests/base-view.js
+++ test/unit/tests/base-view.js
@@ -9,15 +9,12 @@ define(['modules/core/base-view'], function(BaseView) {
 
     suite('#render', function() {
       test('invokes custom `afterRender` hook', function() {
-        var callCount = 0;
-        this.view.afterRender = function() {
-          callCount++;
-        };
+        this.view.afterRender = sinon.spy();
         this.view.template = function() {};
 
         this.view.render();
 
-        assert.equal(callCount, 1);
+        assert.equal(this.view.afterRender.callCount, 1);
       });
 
       test('correctly sets the markup according to the template', function() {
@@ -34,33 +31,28 @@ define(['modules/core/base-view'], function(BaseView) {
         'expands the template with the data returned by `serializeData`',
         function() {
           var expectedData = {};
-          var actualData;
           this.view.serializeData = function() {
             return expectedData;
           };
-          this.view.template = function(data) {
-            actualData = data;
-          };
+          this.view.template = sinon.spy();
+
           this.view.render();
 
-          assert.equal(actualData, expectedData);
+          assert.equal(this.view.template.args[0][0], expectedData);
         }
       );
     });
 
     suite('#destroy', function() {
       test('all event listeners are unbound', function() {
-        var callCount = 0;
-        var handler = function() {
-          callCount++;
-        };
+        var handler = sinon.spy();
         var view2 = new BaseView();
         this.view.listenTo(view2, 'sample-event', handler);
 
         this.view.destroy();
         view2.trigger('sample-event');
 
-        assert.equal(callCount, 0);
+        assert.equal(handler.callCount, 0);
       });
 
       test('container element is emptied', function() {
