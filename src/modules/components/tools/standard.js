d0b67f35c84d256b91da76a500afb9b8678235d7
Mike Pennisi <mike@mikepennisi.com>
2014-09-03 12:44:04 -0400

    Degrade gracefully in legacy browsers
    Remove the "Webcam" button from the user interface in contexts where the
    `getUserMedia` API is not available.

--- src/modules/components/tools/standard.js
+++ src/modules/components/tools/standard.js
@@ -2,10 +2,17 @@ define(function(require) {
   'use strict';
 
   var BaseView = require('modules/core/base-view');
+  var gum = require('modules/services/gum-compat');
   var template = require('tmpl!modules/components/tools/standard');
 
   return BaseView.extend({
-    template: template
+    template: template,
+
+    afterRender: function() {
+      if (!gum.getUserMedia) {
+        this.$('.btn').eq(0).remove();
+      }
+    }
   });
 
 });
